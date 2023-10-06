66639b40cfe10cf90b29832dead64e3250e2c39a
Pushpakant Shindeclc <pushpakantshinde@gmail.com>
2023-10-06 21:33:36 +0530

    modules added

--- main.py
+++ main.py
@@ -1,3 +1,25 @@
-from menu import Menu
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
 
 menu = Menu()
+coffee_maker = CoffeeMaker()
+money_machine = MoneyMachine()
+
+should_continue = True
+
+while should_continue:
+    options = menu.get_items()
+    choice = input(f"What would you like? {options}: ")
+    if choice == 'espresso' or choice == 'latte' or choice == 'cappuccino':
+        drink = menu.find_drink(choice)
+        should_continue = coffee_maker.is_resource_sufficient(drink)
+        if should_continue:
+            if money_machine.make_payment(drink.cost):
+                coffee_maker.make_coffee(drink)
+
+    elif choice == 'report':
+        coffee_maker.report()
+        money_machine.report()
+    elif choice == 'off':
+        should_continue = False
--- menu.py
+++ menu.py
@@ -19,4 +19,17 @@ class Menu:
             MenuItem(name="cappuccino", water=250, milk=50, coffee=24, cost=3),
         ]
 
+    def get_items(self):
+        """Returns all the names of the available menu items"""
+        options = ""
+        for item in self.menu:
+            options += f"{item.name}/"
+        return options
+
+    def find_drink(self, order_name):
+        """Searches the menu for a particular drink by name. Returns that item if it exists, otherwise returns None"""
+        for item in self.menu:
+            if item.name == order_name:
+                return item
+        print("Sorry that item is not available.")
 
